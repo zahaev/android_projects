9b5ac12ed4cfec1eb79c8e91e402f7306410e288
whow <whow6035@gmail.com>
2026-03-07 15:00:56 +0300

    feat:Добавлен репозиторий (рефакторинг слоя данных)

---
+++ app/src/main/java/com/example/myapplication/model/data/repository/CharacterRepositoryImpl.kt
@@ -0,0 +1,105 @@
+// model/data/repository/CharacterRepositoryImpl.kt
+package com.example.myapplication.model.data.repository
+
+import android.util.Log
+import com.example.myapplication.model.domain.Character
+import com.example.myapplication.model.domain.repository.CharacterRepository
+import com.example.myapplication.model.domain.CharacterUi
+import com.example.myapplication.model.data.local.CharacterLocalDataSource
+import com.example.myapplication.model.data.remote.CharacterRemoteDataSource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import com.example.myapplication.model.data.mapper.toDomain
+import com.example.myapplication.model.data.mapper.mapCharacterDtoToEntity
+import com.example.myapplication.model.data.mapper.toEntity
+import com.example.myapplication.model.data.mapper.toUi
+import com.example.myapplication.model.data.mapper.toDomain as characterEntityToDomain
+import com.example.myapplication.model.data.mapper.toDomain as favoriteEntityToDomain
+import com.example.myapplication.model.data.mapper.toFavoriteEntity
+
+//Спрашивает Local
+//Если пусто → идёт в Remote
+//Сохраняет в БД
+//Возвращает Domain
+class CharacterRepositoryImpl(
+    private val localDataSource: CharacterLocalDataSource,
+    private val remoteDataSource:CharacterRemoteDataSource
+) : CharacterRepository {
+
+    override suspend fun getCharactersPage(
+        page: Int,
+        pageSize: Int
+    ): List<Character> = withContext(Dispatchers.IO) {
+        val offset = page * pageSize
+        val localData = localDataSource.getCharactersPage(offset, pageSize)
+        if (localData.isEmpty()) {
+            try {
+                Log.d("Repo", "Loading from network, page=$page")
+                val remoteData: List<com.example.myapplication.model.data.remote.CharacterDto>
+                = remoteDataSource.getCharacters(page+1)// List<CharacterDto>
+                val entities = remoteData.map { mapCharacterDtoToEntity(it) }//mapper CharacterDto -> CharacterEntity
+                localDataSource.insertAll(entities)
+            } catch (e: Exception) {
+                Log.e("Repo", "Network load failed: ${e.message}", e)
+                // можно залогировать позже
+            }
+
+       
+        }
+       localDataSource.getCharactersPage(offset, pageSize)
+           .map { it.toDomain() }
+
+
+
+    }
+    override suspend fun getFavorites(): List<Character> = withContext(Dispatchers.IO) {
+        localDataSource.getAllFavorites().map { it.toDomain() }
+    }
+
+    override suspend fun isFavorite(id: Int): Boolean = withContext(Dispatchers.IO) {
+        localDataSource.isFavorite(id)
+    }
+
+    override suspend fun toggleFavorite(character: Character) = withContext(Dispatchers.IO) {
+        val exists = localDataSource.isFavorite(character.id)
+        if (exists) {
+            localDataSource.deleteFavorite(character.id)
+        } else {
+            localDataSource.insertFavorite(character.toFavoriteEntity())
+        }
+    }
+
+    override suspend fun getCharacterById(id: Int): Character? =
+        withContext(Dispatchers.IO) {
+            localDataSource.getCharacterById(id)?.toDomain()
+        }
+    override suspend fun getCharactersPageUi(page: Int, pageSize: Int): List<CharacterUi> =
+        withContext(Dispatchers.IO) {
+            val offset = page * pageSize
+
+            val local = localDataSource.getCharactersPageWithFavorite(offset, pageSize)
+
+            if (local.isEmpty()) {
+                try {
+                    val remote = remoteDataSource.getCharacters(page + 1)
+                    val entities = remote.map { mapCharacterDtoToEntity(it) }
+                    localDataSource.insertAll(entities)
+                } catch (e: Exception) {
+                    Log.e("Repo", "Network load failed: ${e.message}", e)
+                }
+            }
+
+            localDataSource.getCharactersPageWithFavorite(offset, pageSize)
+                .map { it.toUi() }
+        }
+
+    override suspend fun addCharacter(character: Character) =
+        withContext(Dispatchers.IO) {
+            localDataSource.insert(character.toEntity())
+        }
+
+    override suspend fun deleteCharacter(id: Int) =
+        withContext(Dispatchers.IO) {
+            localDataSource.delete(id)
+        }
+}
---
+++ app/src/main/java/com/example/myapplication/model/domain/repository/CharacterRepository.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication.model.domain.repository
+import com.example.myapplication.model.domain.Character
+import com.example.myapplication.model.domain.CharacterUi
+
+//интерфейс
+interface CharacterRepository {
+    suspend fun getCharactersPage(page: Int, pageSize: Int): List<Character>
+
+    suspend fun getCharactersPageUi(page: Int, pageSize: Int): List<CharacterUi>
+
+    suspend fun getCharacterById(id: Int): Character?
+
+    suspend fun getFavorites(): List<Character>
+
+    suspend fun toggleFavorite(character: Character)
+
+    suspend fun isFavorite(id: Int): Boolean
+
+    suspend fun addCharacter(character: Character)
+
+    suspend fun deleteCharacter(id: Int)
+
+}
