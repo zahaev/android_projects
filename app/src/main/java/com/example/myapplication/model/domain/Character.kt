2c7ae311410aafc8192683bbf4989fa25d11fcca
whow <whow6035@gmail.com>
2026-03-14 14:44:08 +0300

    fix:убран лишний функционал класса

--- app/src/main/java/com/example/myapplication/model/domain/Character.kt
+++ app/src/main/java/com/example/myapplication/model/domain/Character.kt
@@ -1,38 +1,18 @@
 package com.example.myapplication.model.domain
 
-import android.os.Parcelable
-import android.os.Parcel
-import android.os.Parcelable.Creator
-
+import com.example.myapplication.model.data.local.ApiLocation
 
 data class Character(
     val id: Int,
     val name: String,
-    val age: String,
-    val imageUrl: String,
-    val description: String
-) : Parcelable {
-
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString()?:""
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(id)
-        parcel.writeString(name)
-        parcel.writeString(age)
-        parcel.writeString(imageUrl)
-        parcel.writeString(description)
-    }
-
-    override fun describeContents(): Int = 0
-
-    companion object CREATOR : Creator<Character> {
-        override fun createFromParcel(parcel: Parcel): Character = Character(parcel)
-        override fun newArray(size: Int): Array<Character?> = arrayOfNulls(size)
-    }
-}
+    val status: String,
+    val species: String,
+    val type: String,
+    val gender: String,
+    val origin: ApiLocation,
+    val location: ApiLocation,
+    val image: String,
+    val episode: List<String>,
+    val url: String,
+    val created: String
+)
